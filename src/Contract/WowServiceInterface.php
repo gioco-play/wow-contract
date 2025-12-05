52670d726b03ec5962c8a13de6185cdfc93ecea3
tkd9714 <chunwei.liu@goldenf.co>
2025-12-05 10:28:56 +0800

    first commit

---
+++ src/Contract/WowServiceInterface.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlus\WOW\Contract;
+/**
+ * 交易
+ */
+interface WowServiceInterface {
+
+    /**
+     * 遊戲啟動
+     *
+     * @param string $op_code
+     * @param string $account
+     * @param string $game_code
+     * @param string $language
+     * @param string $back_url
+     * @return mixed
+     */
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $back_url = null);
+    
+    /**
+     * 遊戲詳情
+     * @param string $op_code
+     * @param string $account
+     * @param string $bet_id
+     * @return mixed
+     */
+    function gameDetail(string $op_code, string $account, string $bet_id);
+
+}
+
